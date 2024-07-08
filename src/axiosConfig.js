7c444c39968d36285f5605785f1fb8bff4a29cb6
methsaradissanayake <155160131+methsaradissanayake@users.noreply.github.com>
2024-07-08 10:07:55 +0530

    fix error

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-  baseURL:'https://agrariantradesystemapi.azurewebsites.net/api',
-  // baseURL:'https://localhost:7144/api',
+  // baseURL:'https://agrariantradesystemapi.azurewebsites.net/api',
+  baseURL:'https://localhost:7144/api',
 });
 
 export default axiosInstance;
